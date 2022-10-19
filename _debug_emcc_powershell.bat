68daa4face1cdeab175a9aba3e5056cb587b78ca
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2022-10-18 23:09:44 -0400

    adjust naming of batch files

--- _debug_emcc_powershell.bat
+++ _debug_emcc_powershell.bat
@@ -1,6 +1,6 @@
 powershell -NoExit Add-Content -path (Get-PSReadlineOption).HistorySavePath 'cls; emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH'
 :: powershell -NoExit opens powershell without exiting after the command is complete
-:: NOTE: emsdk must be activated permanently by running .\emsdk activate latest --permanent
 :: Add-Content -path (Get-PSReadlineOption).HistorySavePath adds the command to be run to the console history so that it can be run more easily
 :: cls; to clear the output of the previous compile
-:: 'emcc path\to\cpp\file -o path\of\output\js\file -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -fmodule-map-file=''kynedraw.modulemap'' -fmodules' compiles the file with debug commands
+:: emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH compiles the file with debug commands, without optimizations
+:: NOTE: emsdk must be activated permanently by running .\emsdk activate latest --permanent
---
+++ _release_emcc_powershell.bat
@@ -0,0 +1,6 @@
+powershell -NoExit Add-Content -path (Get-PSReadlineOption).HistorySavePath 'cls; emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -sALLOW_MEMORY_GROWTH -O3'
+:: powershell -NoExit opens powershell without exiting after the command is complete
+:: Add-Content -path (Get-PSReadlineOption).HistorySavePath adds the command to be run to the console history so that it can be run more easily
+:: cls; to clear the output of the previous compile
+:: emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -sALLOW_MEMORY_GROWTH -O3 compiles the file without debug commands, with optimization
+:: NOTE: emsdk must be activated permanently by running .\emsdk activate latest --permanent
