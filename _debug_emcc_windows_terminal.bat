ec3611880657e4e48d66c4799f50c7d6219f77a7
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2022-10-25 21:54:45 -0400

    update compile scripts

--- _debug_emcc_windows_terminal.bat
+++ _debug_emcc_windows_terminal.bat
@@ -1,7 +1,7 @@
-wt -d %~dp0 powershell -NoExit Add-Content -path (Get-PSReadlineOption).HistorySavePath 'cls\; emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH'
+wt -d %~dp0 powershell -NoExit Add-Content -path (Get-PSReadlineOption).HistorySavePath 'cls\; emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH -sPTHREAD_POOL_SIZE=2 -pthread'
 :: wt -d %~dp0 opens Windows Terminal to the current directory
 :: powershell -NoExit opens powershell without exiting after the command is complete
 :: Add-Content -path (Get-PSReadlineOption).HistorySavePath adds the command to be run to the console history so that it can be run more easily
 :: cls\; to clear the output of the previous compile, with the semicolon escaped with a backslash so that windows terminal does not interpret it as another command
-:: emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH compiles the file with debug commands, without optimizations
+:: emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH -sPTHREAD_POOL_SIZE=2 -pthread compiles the file with debug commands, without optimizations
 :: NOTE: emsdk must be activated permanently by running .\emsdk activate latest --permanent
