af49f9fa70c90023e22593503f4f3d62d6d01e58
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2022-11-06 00:37:10 -0400

    update batch files for fftw3

--- _debug_emcc_powershell.bat
+++ _debug_emcc_powershell.bat
@@ -1,6 +1,6 @@
-powershell -NoExit Add-Content -path (Get-PSReadlineOption).HistorySavePath 'cls; emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH -sPTHREAD_POOL_SIZE=2 -pthread'
+powershell -NoExit Add-Content -path (Get-PSReadlineOption).HistorySavePath 'cls; emcc fftw-3.3.10/libs/libfftw3.a main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH -sPTHREAD_POOL_SIZE=2 -pthread'
 :: powershell -NoExit opens powershell without exiting after the command is complete
 :: Add-Content -path (Get-PSReadlineOption).HistorySavePath adds the command to be run to the console history so that it can be run more easily
 :: cls; to clear the output of the previous compile
-:: emcc main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH -sPTHREAD_POOL_SIZE=2 -pthread compiles the file with debug commands, without optimizations
+:: emcc fftw-3.3.10/libs/libfftw3.a main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -g -sNO_DISABLE_EXCEPTION_CATCHING -sALLOW_MEMORY_GROWTH -sPTHREAD_POOL_SIZE=2 -pthread compiles the file with debug commands, without optimizations
 :: NOTE: emsdk must be activated permanently by running .\emsdk activate latest --permanent
