c6f3461f4bfe5dfefebf0047c93429de6686aa47
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2023-11-07 13:54:35 -0500

    Update _simple_cors_http_server.py

--- _simple_cors_http_server.py
+++ _simple_cors_http_server.py
@@ -7,6 +7,7 @@ class CORSRequestHandler (SimpleHTTPRequestHandler):
     def end_headers (self):
         self.send_header('Cross-Origin-Opener-Policy','same-origin')
         self.send_header('Cross-Origin-Embedder-Policy','require-corp')
+        self.send_header('Cross-Origin-Resource-Policy','same-origin')
         SimpleHTTPRequestHandler.end_headers(self)
 
 if __name__ == '__main__':
