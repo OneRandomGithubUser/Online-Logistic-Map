aed86e3b1c5c3bd4c240e190dad87e5e92dc11fa
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2023-11-07 12:20:18 -0500

    Update _release_emcc_windows_terminal.bat

--- _release_emcc_windows_terminal.bat
+++ _release_emcc_windows_terminal.bat
@@ -5,3 +5,4 @@ wt -d %~dp0 powershell -NoExit Add-Content -path (Get-PSReadlineOption).HistoryS
 :: cls\; to clear the output of the previous compile, with the semicolon escaped with a backslash so that windows terminal does not interpret it as another command
 :: emcc fftw-3.3.10/libs/libfftw3.a main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -lembind -sALLOW_MEMORY_GROWTH -O3 -sPTHREAD_POOL_SIZE=2 -pthread compiles the file without debug commands, with optimization
 :: NOTE: emsdk must be activated permanently by running .\emsdk activate latest --permanent
+:: WARNING: this will throw an "Add-Content : Could not find a part of the path" error if PowerShell has never been run on the machine before.
