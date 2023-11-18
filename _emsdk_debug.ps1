911ccef906958065d8813da60c14392bc64247a1
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2023-11-17 22:42:28 -0500

    Update compilation scripts

--- _emsdk_debug.ps1
+++ _emsdk_debug.ps1
@@ -9,5 +9,5 @@ Set-ExecutionPolicy RemoteSigned -Scope Process
 Set-ExecutionPolicy $execution_policy -Scope Process
 cd ..
 echo "Compiling started..."
-emcc fftw-3.3.10/libs/libfftw3.a main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -l embind -pthread -s PTHREAD_POOL_SIZE=1 -s ALLOW_MEMORY_GROWTH -O3
+emcc fftw-3.3.10/libs/libfftw3.a main.cpp -o main.js -s USE_BOOST_HEADERS=1 -std=c++20 -l embind -pthread -s PTHREAD_POOL_SIZE=1 -s ALLOW_MEMORY_GROWTH -g -s NO_DISABLE_EXCEPTION_CATCHING
 Pause
