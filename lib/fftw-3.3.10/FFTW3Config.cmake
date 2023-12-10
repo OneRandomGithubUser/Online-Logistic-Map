90a4e3298500528a27edf00218de9d3b5a19fecc
OneRandomGithubUser <60021548+OneRandomGithubUser@users.noreply.github.com>
2023-12-10 18:22:37 -0500

    Move fftw library to lib

--- lib/fftw-3.3.10/FFTW3Config.cmake
+++ lib/fftw-3.3.10/FFTW3Config.cmake
@@ -1,10 +1,10 @@
 # defined since 2.8.3
 if (CMAKE_VERSION VERSION_LESS 2.8.3)
-  get_filename_component (CMAKE_CURRENT_LIST_DIR ${CMAKE_CURRENT_LIST_FILE} PATH)
+  get_filename_component (CMAKE_CURRENT_LIST_DIR FFTW3Config.cmake PATH)
 endif ()
 
 # Allows loading FFTW3 settings from another project
-set (FFTW3_CONFIG_FILE "${CMAKE_CURRENT_LIST_FILE}")
+set (FFTW3_CONFIG_FILE "FFTW3Config.cmake")
 
 set (FFTW3_LIBRARIES fftw3)
 set (FFTW3_LIBRARY_DIRS /usr/local/lib)
--- scripts/upstream/emcc_command_debug.bat
+++ scripts/upstream/emcc_command_debug.bat
@@ -1,4 +1,4 @@
-SET emcc_main=emcc fftw-3.3.10/libs/libfftw3.a src/main.cpp -o build/main.js
+SET emcc_main=emcc lib/fftw-3.3.10/libs/libfftw3.a src/main.cpp -o build/main.js
 SET emcc_lib=-s USE_BOOST_HEADERS=1 -std=c++20 -l embind -pthread
 SET emcc_options=-s PTHREAD_POOL_SIZE=1 -s ALLOW_MEMORY_GROWTH -s NO_DISABLE_EXCEPTION_CATCHING
 SET emcc_optimization=-g
